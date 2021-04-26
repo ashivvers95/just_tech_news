a9c425a300b194044b4dd7ca11cf83001d2256a7
Shivvers <ashivvers95@gmail.com>
2021-04-26 11:05:13 -0700

    password

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
-
-// create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize('just_tech_news_db', 'root', '$nowBear95', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
